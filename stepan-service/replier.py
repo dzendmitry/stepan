b2c4cde491a617be534266796035fd8d04bff0ff
dzendmitry <dzendmitry@gmail.com>
2019-04-13 16:51:42 +0300

    replier refactoring

--- stepan-service/replier.py
+++ stepan-service/replier.py
@@ -24,23 +24,29 @@ def run():
             print("RESPONSE: Noise")
         elif command == Commands.STEPAN:
             print("RESPONSE: Yes, My Lord!")
+            if channel is not None:
+                send_command(Commands.STEPAN, loop)
         elif command == Commands.SHOW_CATALOG_API:
             print("RESPONSE: Showing CATALOG API dashboard")
             if channel is not None:
-                loop.run_until_complete(send_command(str(Commands.SHOW_CATALOG_API)))
+                send_command(Commands.SHOW_CATALOG_API, loop)
         elif command == Commands.SHOW_K8S_API:
             print("RESPONSE: Showing K8S dashboard")
             if channel is not None:
-                loop.run_until_complete(send_command(str(Commands.SHOW_K8S_API)))
+                send_command(Commands.SHOW_K8S_API, loop)
         elif command == Commands.SHOW_ITEM_API:
             print("RESPONSE: Showing ITEM API dashboard")
             if channel is not None:
-                loop.run_until_complete(send_command(str(Commands.SHOW_ITEM_API)))
+                send_command(Commands.SHOW_ITEM_API, loop)
         else:
             print("RESPONSE: Unknown command")
 
 
-async def send_command(cmd):
+def send_command(cmd, loop):
+    loop.run_until_complete(send_command_async(str(cmd)))
+
+
+async def send_command_async(cmd):
     channel.send(cmd)
 
 
